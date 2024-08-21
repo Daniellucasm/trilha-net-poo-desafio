e4a51e45e07e29ad06efdab2293420d80be80fc5
Daniellucasm <danielmurta123@gmail.com>
2024-08-20 22:48:54 -0300

    Resolução do Desafio Dio.me

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia:");
+Smartphone nokia = new Nokia("9999", "A20", "1234", 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Celular Iphone:");
+Smartphone iphone = new Iphone("1234", "15 PRO", "3213", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
